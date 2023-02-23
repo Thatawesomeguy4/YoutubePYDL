5726b494d47aefd56981a1ea372aa6c2e727ac51
derek <derekteepee@gmail.com>
2023-02-23 15:49:12 -0500

    added some UI stuff

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -5,7 +5,7 @@ video_list_column = [
     [
         sg.Text("Image Folder"),
         sg.In(size=(25, 1), enable_events=True, key="-URL-"),
-        sg.Button("Get Videos"),
+        sg.Button("Get Videos"),  # need to add logic to this button to search for a video based on the entered URL
     ],
     [
         sg.Listbox(
@@ -14,9 +14,27 @@ video_list_column = [
     ],
 ]
 
-# For now will only show the name of the file that was chosen
+# For now will only show the name of the video that was chosen
 video_viewer_column = [
-    [sg.Text("Choose an image from list on left:")],
+    [sg.Text("Choose a video from list on left:")],
     [sg.Text(size=(40, 1), key="-TOUT-")],
     [sg.Image(key="-VIDEO-")],
 ]
+
+# ----- Full layout -----
+layout = [
+    [
+        sg.Column(video_list_column),
+        sg.VSeperator(),
+        sg.Column(video_viewer_column),
+    ]
+]
+
+window = sg.Window("Image Viewer", layout)
+
+
+
+while True:
+    event, values = window.read()
+    if event == "Exit" or event == sg.WIN_CLOSED:
+        break
